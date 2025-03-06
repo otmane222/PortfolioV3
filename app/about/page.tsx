9fcd5083493981068c0e6a727d305339aa2f032f
otmane222 <azertyuioppdh@gmail.com>
2025-03-06 15:00:18 +0000

    education added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -34,7 +34,7 @@ export default function About() {
     // }
       className="mt-[180px] w-full h-auto bg-slate-400"
     >
-      <div className={`w-full relative  z-10 flex ${theme == 'dark' ? "bg-dark-bg" : "bg-light-bg"} justify-center  h-full overflow- `}>
+      <div className={`w-full relative font-[tommy2]  z-10 flex ${theme == 'dark' ? "bg-dark-bg" : "bg-light-bg"} justify-center  h-full overflow- `}>
         <div className="w-full max-w-[1240px] b-red-800    h-[5000px]">
           
           <div className="w-full h-[400px] b-red-500 flex justify-center items-center">
@@ -48,9 +48,9 @@ export default function About() {
           </div>
 
 
-          <h1 className="text-4xl font-bold text-center">Education</h1>
+          <h1 className="text-4xl font-bold text-center pb-[30px] pt-[30px]">Education</h1>
           
-          <div className="w-full h-[600px] bg-blue-700 flex  justify-center items-center">
+          <div className="w-full h-[600px] b-blue-700 flex  justify-center items-center">
             
               <div className="flex flex-col h-[500px] w-auto">
                 <div className="w-full flex h-[150px] mt-[10px] mb-[10px]  b-emerald-500">
@@ -58,27 +58,28 @@ export default function About() {
                   
                 </div>
 
-                <div className="w-full flex h-[150px] mt-[10px] mb-[10px]  bg-emerald-500">
-                  <div className="w-[30px] h-full bg-red-300"></div>
-                  <div className="w-auto] h-[30px] bg-purple-500">
-                    <h1>Hight Scholl Diplom</h1>
+                <div className="w-full flex h-[150px] mt-[10px] mb-[10px]  b-emerald-500">
+                  <div className="w-auto h-auto b-purple-500 ">
+                    <h1 className="font-[tommy] text-[20px]" >Hight School Diplom</h1>
                     <h1>Science Math</h1>
                     <p>2006-2019</p>
-                    <p>Ifran, Allal El Fasi</p>
+                    <p>Ifran, Lycce Allal El Fasi</p>
                   </div>
+                  <div className={`w-[30px] h-[30px] ${theme == 'dark' ? "bg-light-bg" : "bg-dark-bg"} rounded-full ml-2 mr-2 `}></div>
                 </div>
               </div>
-              <div className="h-full w-[30px] bg-[#d5cd76] "></div>
+
+              <div className={`h-full w-[20px] ml-2 mr-2 rounded-full ${theme == 'dark' ? "bg-light-bg" : "bg-dark-bg"}`}></div>
               
               <div className="flex flex-col h-[500px] w-auto">
 
-                <div className="w-full flex h-[150px] mt-[10px] mb-[10px]  bg-emerald-500">
-                  <div className="w-[30px] h-full bg-red-300"></div>
-                  <div className="w-auto h-[30px] bg-purple-500">
-                    <h1>Hight Scholl Diplom</h1>
-                    <h1>Science Math</h1>
-                    <p>2006-2019</p>
-                    <p>Ifran, Allal El Fasi</p>
+                <div className="w-full flex h-[150px] mt-[10px] mb-[10px]  b-emerald-500">
+                  <div className={`w-[30px] h-[30px] ${theme == 'dark' ? "bg-light-bg" : "bg-dark-bg"} rounded-full ml-2 mr-2 `}></div>
+                  <div className="w-auto h-auto b-purple-500">
+                    <h1 className="font-[tommy] text-[20px]">Hight Scholl Diplom</h1>
+                    <h1>Science Math Applique et informatics</h1>
+                    <p>2020-2022</p>
+                    <p>Meknes, university Moulay Ismail</p>
                   </div>
                 </div>
                 
@@ -86,13 +87,13 @@ export default function About() {
                   
                 </div>
 
-                <div className="w-full flex h-[150px] mt-[10px] mb-[10px]  bg-emerald-500">
-                  <div className="w-[30px] h-full bg-red-300"></div>
-                  <div className="w-auto h-[30px] bg-purple-500">
-                    <h1>Hight Scholl Diplom</h1>
-                    <h1>Science Math</h1>
-                    <p>2006-2019</p>
-                    <p>Ifran, Allal El Fasi</p>
+                <div className="w-full flex h-[150px] mt-[10px] mb-[10px]  b-emerald-500">
+                  <div className={`w-[30px] h-[30px] ${theme == 'dark' ? "bg-light-bg" : "bg-dark-bg"} rounded-full ml-2 mr-2 `}></div>
+                  <div className="w-auto h-auto b-purple-500">
+                    <h1 className="font-[tommy] text-[20px]">Common Core (42 Network)</h1>
+                    <h1>IT Architect</h1>
+                    <p>2022-2024</p>
+                    <p>Khouribga, 1337 coding school</p>
                   </div>
                 </div>
               </div>
