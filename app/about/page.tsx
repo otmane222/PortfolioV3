86cd23264cc5be8ffa7c35f16961f350f38695e6
otmane222 <azertyuioppdh@gmail.com>
2025-03-07 04:04:22 +0000

    education edits

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,37 +1,79 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, useAnimate } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { ApearanceContext } from "@/app/context/Themecontext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
+
+type AnimatedBoxProps = {
+  text: string; // To pass different content if needed
+};
+
+const AnimatedBox = ({ children }: { children: React.ReactNode}) => {
+  const [hover, setHover] = useState(false);
+  const [animate, controls] = useAnimate();
+  // const animateRef = useRef(null); // Unique ref for each component
+
+  const handleAnimateIn = async () => {
+    await controls(animate.current, {x: 0})
+    await controls(animate.current, {
+      height: 130,
+      transition: { duration: 0.3, ease: [0.16, 1, 0.3, 1] },
+    });
+    await controls(animate.current, {
+      width: 250,
+      transition: { duration: 0.3, ease: [0.16, 1, 0.3, 1] },
+    });
+    await controls("#hover", { opacity: 1, y: 0});
+
+    setHover(true);
+  };
+
+  const handleAnimateOut = async () => {
+    await controls("#hover", { opacity: 0 });
+    await controls(animate.current, {
+      width: 30,
+      transition: { duration: 0.3, ease: [0.16, 1, 0.3, 1] },
+    });
+    await controls(animate.current, {
+      height: 30,
+      transition: { duration: 0.3, ease: [0.16, 1, 0.3, 1] },
+    });
+    await controls(animate.current, {x: window.innerWidth/10})
+    await controls("#hover", { y: 20 });
+    setHover(false);
+  };
+
+  const { theme } = useContext(ApearanceContext) || {};
+  return (
+  
+    <motion.div
+      ref={animate}
+      className={`w-[30px] h-[30px] cursor-pointer ${
+        theme === "dark" ? "bg-light-bg" : "bg-dark-bg"
+      } rounded-[20px] ml-2 mr-2 flex items-center justify-center overflow-hidden`}
+      onClick={hover ? handleAnimateOut : handleAnimateIn}
+    >
+      <motion.div
+        id="hover"
+        className={`flex flex-col   ${theme == 'dark' ? "text-dark-bg" : "text-light-bg"} pl-4 pt-3 w-full h-full  text-[#]  opacity-0`}
+        initial={{ y: 20, opacity: 0 }}
+      >
+        {children}
+      </motion.div>
+    </motion.div>
+  );
+};
 
 export default function About() {
   const { theme } = useContext(ApearanceContext) || {}
   const pathname = usePathname(); // Get current route
-  const pageVariants = {
-    initial: { opacity: 0, y: 20 },
-    animate: { opacity: 1, y: 0 },
-    exit: { opacity: 0, y: -20 },
-  };
+
   
   return (
   <motion.div
-    // initial="initial"
-    // animate="animate"
-    // // exit="exit"
-    // variants={pageVariants}
-    // transition={{ duration: 0.5 }}
-    // style={{ willChange: "opacity, transform" }}
-    // id="page"
-    // onAnimationComplete={() => {
-    //   // window.scrollTo(0, 0);
-    //   console.log("Page transition complete");
-    // }}
-    // onAnimationStart={() => {
-    //   console.log("Page transition started");
-    // }
-    // }
+
       className="mt-[180px] w-full h-auto bg-slate-400"
     >
       <div className={`w-full relative font-[tommy2]  z-10 flex ${theme == 'dark' ? "bg-dark-bg" : "bg-light-bg"} justify-center  h-full overflow- `}>
@@ -50,51 +92,39 @@ export default function About() {
 
           <h1 className="text-4xl font-bold text-center pb-[30px] pt-[30px]">Education</h1>
           
-          <div className="w-full h-[600px] b-blue-700 flex  justify-center items-center">
+          <div className="w-full h-[500px] b-blue-700   justify-center items-center">
             
-              <div className="flex flex-col h-[500px] w-auto">
-                <div className="w-full flex h-[150px] mt-[10px] mb-[10px]  b-emerald-500">
-                  
-                  
-                </div>
-
-                <div className="w-full flex h-[150px] mt-[10px] mb-[10px]  b-emerald-500">
-                  <div className="w-auto h-auto b-purple-500 ">
-                    <h1 className="font-[tommy] text-[20px]" >Hight School Diplom</h1>
-                    <h1>Science Math</h1>
-                    <p>2006-2019</p>
-                    <p>Ifran, Lycce Allal El Fasi</p>
-                  </div>
-                  <div className={`w-[30px] h-[30px] ${theme == 'dark' ? "bg-light-bg" : "bg-dark-bg"} rounded-full ml-2 mr-2 `}></div>
-                </div>
-              </div>
-
-              <div className={`h-full w-[20px] ml-2 mr-2 rounded-full ${theme == 'dark' ? "bg-light-bg" : "bg-dark-bg"}`}></div>
               
-              <div className="flex flex-col h-[500px] w-auto">
+              <div className="flex 800:flex-row flex-col h-[500px] w-auto">
 
-                <div className="w-full flex h-[150px] mt-[10px] mb-[10px]  b-emerald-500">
-                  <div className={`w-[30px] h-[30px] ${theme == 'dark' ? "bg-light-bg" : "bg-dark-bg"} rounded-full ml-2 mr-2 `}></div>
-                  <div className="w-auto h-auto b-purple-500">
-                    <h1 className="font-[tommy] text-[20px]">Hight Scholl Diplom</h1>
-                    <h1>Science Math Applique et informatics</h1>
-                    <p>2020-2022</p>
-                    <p>Meknes, university Moulay Ismail</p>
-                  </div>
+                <div className="w-[250px] mb-4 flex h-[130px]">  
+
+                  <AnimatedBox>
+                      <h1 className="font-[tommy] text-[20px]">High School Diploma</h1>
+                      <h1>Science Math</h1>
+                      <p>2006-2019</p>
+                      <p>Ifrane, Lycée Allal El Fasi</p>
+                  </AnimatedBox>
                 </div>
                 
-                <div className="w-full flex h-[150px] mt-[10px] mb-[10px]  b-emerald-500">
-                  
+                <div className="w-[250px] mb-4 flex h-[130px]">  
+
+                  <AnimatedBox>
+                      <h1 className="font-[tommy] text-[20px]">High School Diploma</h1>
+                      <h1>Science Math</h1>
+                      <p>2006-2019</p>
+                      <p>Ifrane, Lycée Allal El Fasi</p>
+                  </AnimatedBox>
                 </div>
 
-                <div className="w-full flex h-[150px] mt-[10px] mb-[10px]  b-emerald-500">
-                  <div className={`w-[30px] h-[30px] ${theme == 'dark' ? "bg-light-bg" : "bg-dark-bg"} rounded-full ml-2 mr-2 `}></div>
-                  <div className="w-auto h-auto b-purple-500">
-                    <h1 className="font-[tommy] text-[20px]">Common Core (42 Network)</h1>
-                    <h1>IT Architect</h1>
-                    <p>2022-2024</p>
-                    <p>Khouribga, 1337 coding school</p>
-                  </div>
+                <div className="w-[255px] flex h-[130px]">  
+
+                  <AnimatedBox>
+                      <h1 className="font-[tommy] text-[20px]">High School Diploma</h1>
+                      <h1>Science Math</h1>
+                      <p>2006-2019</p>
+                      <p>Ifrane, Lycée Allal El Fasi</p>
+                  </AnimatedBox>
                 </div>
               </div>
 
