1074121656800a30bd55d80bf1f1a3ea9954438f
otmane222 <azertyuioppdh@gmail.com>
2025-03-09 17:41:14 +0000

    responsive footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -37,7 +37,7 @@ export default function Footer() {
     <motion.div
       ref={container}
       style={{y}}
-      className=' h-screen z-0 relative text-white b-black '
+      className=' h-auto z-0 relative text-white b-black '
     >
       
       {/* <svg className=' absolute top-[0] w-full z-[100]  h-[400px]  ' xmlns="http://www.w3.org/2000/svg">
@@ -48,116 +48,111 @@ export default function Footer() {
       </svg> */}
 
 
-      <div className='  bg-[#1d1d1d] h-screen w-full flex justify-center items-center'>
-        <div className='flex justify-center items-center b-[#454444] w-full max-w-[1240px]  h-full'>
-          <div className="flex justify- items- flex-col gap-4 h-full w-[50%] b-sky-700">
-              <div className="flex flex-col b-lime-700 p-10 rounded-xl  max-w-md mx-auto">
-                <h1 className="text-4xl font-bold pb-6 pt-[40px] text-white text-">
-                  Send me a message
-                </h1>
-                <form action="">
-                  <div className="flex flex-col gap-6">
-                    <div className="flex flex-col gap-2">
-                      <label htmlFor="name" className="text-white text-sm font-semibold">
-                        Your Name*
-                      </label>
-                      <input 
-                        className="p-3 rounded-md bg-white text-black focus:outline-none focus:ring-0 focus:ring-none transition" 
-                        type="text" 
-                        id="name" 
-                      />
-                    </div>
-                    <div className="flex flex-col gap-2">
-                      <label htmlFor="email" className="text-white text-sm font-semibold">
-                        Your Email*
-                      </label>
-                      <input 
-                        className="p-3 rounded-md bg-white text-black focus:outline-none focus:ring-0 focus:ring-lime-500 transition" 
-                        type="email" 
-                        id="email" 
-                      />
-                    </div>
-                    <div className="flex flex-col gap-2">
-                      <label htmlFor="message" className="text-white text-sm font-semibold">
-                        Your Message*
-                      </label>
-                      <textarea 
-                        className="p-3 rounded-md bg-white text-black focus:outline-none focus:ring-0 focus:ring-lime-500 transition" 
-                        name="message" 
-                        id="message" 
-                        cols={10} 
-                        rows={6}
-                      ></textarea>
-                    </div>
-                    <button 
-                      className="bg-slate-800 text-white p-3 rounded-md hover:bg-slate-600 transition w-full font-semibold shadow-md">
-                      Send Message
-                    </button>
+      <div className='  bg-[#1d1d1d] h-auto w-full flex justify-center items-center'>
+        <div className='md:flex-row flex md:pt-0 md:pb-0 pt-[150px] pb-[140px] flex-col items-center  md:justify-center md:items-center b-[#454444] w-full max-w-[1240px]  h-full'>
+
+          <div className="flex justify-  flex-col gap-4 md:pb-[0]  h-full w-auto md:w-[50%] b-teal-500">
+            
+            <div className="flex flex-col items-start b-slate-700 h-full w-auto pl-[50px] pt-[100px]">
+              <div className="w-[100px] h-[100px] rounded-full bg-slate-500" >
+              </div>
+
+              <div  className="flex gap-2 h-[90px] items-center b-black">
+                <motion.div style={{rotate, scale: 1.5, x}} className="flex items-center justify-center b-slate-500 w-[40px] h-[40px] rounded-full">
+
+                  <FaArrowTrendDown className="text-3xl text-white" />
+                </motion.div>
+              </div>
+
+              <motion.h1 style={{}} className='text-[12px] opacity-70 font-bold pb-[10px]  '>
+                Contact Details
+              </motion.h1>
+              <div className="flex flex-col gap-1">
+
+
+                <span className="flex flex-col gap-2">
+                  <h1>
+                    +212 6 55 12 11 49
+                  </h1>
+                </span>
+
+                <span className="flex flex-col gap-2">
+                  <h1>
+                    otmaneaboulghit@gmail.com
+                  </h1>
+                </span>
+              </div>
+              <h1 className='text-[12px] opacity-70 font-bold pt-[30px] pb-[10px]  '>Social</h1>
+              <div className="flex flex-col gap-2">
+                <a href="https://twitter.com/otmane_aboulghit" target="_blank" rel="noreferrer">
+                  Linkden
+                </a>
+                <a href="
+                  https://www.linkedin.com/in/otmane-aboulghit-9b4a8b1b7/" target="_blank" rel="noreferrer">
+                  Twitter
+                </a>
+                <a href="
+                  https://www.linkedin.com/in/otmane-aboulghit-9b4a8b1b7/" target="_blank" rel="noreferrer">
+                  Github
+                </a>
+                <a href="
+                  https://www.linkedin.com/in/otmane-aboulghit-9b4a8b1b7/" target="_blank" rel="noreferrer">
+                  Instagram
+                </a>
+                
+              </div>
+              <div className="h-[1px] w-[90%] bg-[#fff] mt-[30px] "></div>
+              <h1 className="text-[14px] pt-2 font-[tommy2]" >Happiness is only real when you share it.</h1>
+            </div>            
+          </div>
+
+          <div className="flex justify- items-center flex-col md:pt-[0] gap-4 h-full w-[90%] md:w-[50%] b-sky-700">
+            <div className="flex flex-col b-lime-700 p-10 rounded-xl  max-w-md md:mx-auto">
+              <h1 className="text-4xl font-bold pb-6 pt-[40px] text-white text-">
+                Send me a message
+              </h1>
+              <form action="">
+                <div className="flex flex-col gap-6">
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="name" className="text-white text-sm font-semibold">
+                      Your Name*
+                    </label>
+                    <input 
+                      className="p-3 rounded-md bg-white text-black focus:outline-none focus:ring-0 focus:ring-none transition" 
+                      type="text" 
+                      id="name" 
+                    />
                   </div>
-                </form>
-              </div>
-
-
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="email" className="text-white text-sm font-semibold">
+                      Your Email*
+                    </label>
+                    <input 
+                      className="p-3 rounded-md bg-white text-black focus:outline-none focus:ring-0 focus:ring-lime-500 transition" 
+                      type="email" 
+                      id="email" 
+                    />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="message" className="text-white text-sm font-semibold">
+                      Your Message*
+                    </label>
+                    <textarea 
+                      className="p-3 rounded-md bg-white text-black focus:outline-none focus:ring-0 focus:ring-lime-500 transition" 
+                      name="message" 
+                      id="message" 
+                      cols={10} 
+                      rows={6}
+                    ></textarea>
+                  </div>
+                  <button 
+                    className="bg-slate-800 text-white p-3 rounded-md hover:bg-slate-600 transition w-full font-semibold shadow-md">
+                    Send Message
+                  </button>
+                </div>
+              </form>
             </div>
-            <div className="flex justify-  flex-col gap-4  h-full w-[50%] b-teal-500">
-              
-              <div className="flex flex-col b-slate-700 h-full w-[90%] pl-[50px] pt-[100px]">
-                <div className="w-[100px] h-[100px] rounded-full bg-slate-500" >
-                </div>
-
-                <div  className="flex gap-2 h-[90px] items-center b-black">
-                  <motion.div style={{rotate, scale: 1.5, x}} className="flex items-center justify-center b-slate-500 w-[40px] h-[40px] rounded-full">
-
-                    <FaArrowTrendDown className="text-3xl text-white" />
-                  </motion.div>
-                </div>
-
-                <motion.h1 style={{}} className='text-[12px] opacity-70 font-bold pb-[10px]  '>
-                  Contact Details
-                </motion.h1>
-                <div className="flex flex-col gap-1">
-  
-
-                  <span className="flex flex-col gap-2">
-                    <h1>
-                      +212 6 55 12 11 49
-                    </h1>
-                  </span>
-
-                  <span className="flex flex-col gap-2">
-                    <h1>
-                      otmaneaboulghit@gmail.com
-                    </h1>
-                  </span>
-                </div>
-                <h1 className='text-[12px] opacity-70 font-bold pt-[30px] pb-[10px]  '>Social</h1>
-                <div className="flex flex-col gap-2">
-                  <a href="https://twitter.com/otmane_aboulghit" target="_blank" rel="noreferrer">
-                    Linkden
-                  </a>
-                  <a href="
-                    https://www.linkedin.com/in/otmane-aboulghit-9b4a8b1b7/" target="_blank" rel="noreferrer">
-                    Twitter
-                  </a>
-                  <a href="
-                    https://www.linkedin.com/in/otmane-aboulghit-9b4a8b1b7/" target="_blank" rel="noreferrer">
-                    Github
-                  </a>
-                  <a href="
-                    https://www.linkedin.com/in/otmane-aboulghit-9b4a8b1b7/" target="_blank" rel="noreferrer">
-                    Instagram
-                  </a>
-                  
-                </div>
-                <div className="h-[1px] w-[90%] bg-[#fff] mt-[30px] "></div>
-                <h1 className="text-[14px] pt-2 font-[tommy2]" >Happiness is only real when you share it.</h1>
-              </div>
-
-              <div>
-
-              </div>
-              
-            </div>
+          </div>
             
         </div>
       </div>
